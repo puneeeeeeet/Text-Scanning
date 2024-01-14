572b4361c229719a8b840902d64c09412f93cf5b
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-01-14 15:31:58 +0530

    restarted building project

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const T = require("tesseract.js")
-T.recognize('./photo.jpg','eng',{ logger: e => console.log(e)})
-    .then(out => console.log(out.data.text))
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const multer = require('multer');
+const {TesseractWorker} = require("tesseract.js")
+const worker = new TesseractWorker();
 
-    // replace "out" in console.log(out) with "out.data.text"
-    // to get only text from data fetched in console.
+const storage = multer.diskStorage({
+    destination: (req,file ,cb)=>{
+        cb(null, "./uploads")
+    },
+    filename: (req,res,cb)=>{
+        cb(null, req.file)
+    }
+})
+const upload = multer ({storage: storage}).single("avatar");
+app.set("view engine", "ejs");
+
+app.get('/',(req,res)=>{
+    res.render("index"); 
+})
+
+app.post("/upload",(req,res)=>{
+    upload(req,res,err =>{
+        console.log(req.file)
+    })
+})
+
+
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, ()=> console.log(`working on port ${PORT}`))
